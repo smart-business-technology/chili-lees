b0cd5a5b36bd42d00ab729711f0c9226ba0abdaa
smart-business-technology <81749573+smart-business-technology@users.noreply.github.com>
2021-09-11 06:58:09 -0400

    Update models.py

--- add_currency/models/models.py
+++ add_currency/models/models.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class add_currency(models.Model):
     _inherit = 'hr.contract'
 
-    curr = fields.Selection(string="", selection=[('dollar', 'الدولار'), ('dinar', 'الدينار العراقي'), ], required=False, )
+    curr = fields.Selection(string="العمله", selection=[('dollar', 'الدولار'), ('dinar', 'الدينار العراقي'), ], required=False, )
