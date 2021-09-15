be039243d34e84c85cc42dd49f969920490bf24c
fatmanedal <fatma-nedal@hotmail.com>
2021-09-15 04:25:39 +0200

    remove mony sign

--- add_currency/models/models.py
+++ add_currency/models/models.py
@@ -7,3 +7,4 @@ class add_currency(models.Model):
     _inherit = 'hr.contract'
 
     curr = fields.Selection(string="العمله", selection=[('dollar', 'الدولار'), ('dinar', 'الدينار العراقي'), ], required=False, )
+    wage = fields.Float('Wage', required=True, tracking=True, help="Employee's monthly gross wage.")
